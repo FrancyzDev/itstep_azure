5c23565ea748a5580ed5ebb37eb1b2415fd5c0e8
Artem Maltsev <artemmaltsevwork@gmail.com>
2026-06-15 13:44:04 +0300

    #2 homework

---
+++ Services/IItemService.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication2.Models;
+namespace WebApplication2.Services;
+
+public interface IItemService
+{
+    Task<IEnumerable<Item>> GetAllItemsAsync();
+    Task<Item?> GetItemByIdAsync(int id);
+    Task<Item> CreateItemAsync(Item item);
+    Task<Item?> UpdateItemAsync(int id, Item item);
+    Task<bool> DeleteItemAsync(int id);
+}
---
+++ Services/ItemService.cs
@@ -0,0 +1,57 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Services;
+
+public class ItemService : IItemService
+{
+    private readonly List<Item> _items = new();
+    private int _nextId = 1;
+
+    public ItemService()
+    {
+        _items.Add(new Item { Id = 1, Name = "Ноутбук", Price = 25000, CreatedAt = DateTime.UtcNow });
+        _items.Add(new Item { Id = 2, Name = "Мишка", Price = 500, CreatedAt = DateTime.UtcNow });
+        _items.Add(new Item { Id = 3, Name = "Клавіатура", Price = 2000, CreatedAt = DateTime.UtcNow });
+        _nextId = 4;
+    }
+
+    public Task<IEnumerable<Item>> GetAllItemsAsync()
+    {
+        return Task.FromResult(_items.AsEnumerable());
+    }
+
+    public Task<Item?> GetItemByIdAsync(int id)
+    {
+        var item = _items.FirstOrDefault(i => i.Id == id);
+        return Task.FromResult(item);
+    }
+
+    public Task<Item> CreateItemAsync(Item item)
+    {
+        item.Id = _nextId++;
+        item.CreatedAt = DateTime.UtcNow;
+        _items.Add(item);
+        return Task.FromResult(item);
+    }
+
+    public Task<Item?> UpdateItemAsync(int id, Item item)
+    {
+        var existingItem = _items.FirstOrDefault(i => i.Id == id);
+        if (existingItem == null)
+            return Task.FromResult<Item?>(null);
+
+        existingItem.Name = item.Name;
+        existingItem.Price = item.Price;
+
+        return Task.FromResult<Item?>(existingItem);
+    }
+
+    public Task<bool> DeleteItemAsync(int id)
+    {
+        var item = _items.FirstOrDefault(i => i.Id == id);
+        if (item == null)
+            return Task.FromResult(false);
+
+        return Task.FromResult(_items.Remove(item));
+    }
+}
