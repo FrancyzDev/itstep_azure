3044637c0d8be82a88918f6027042a3b2877e96c
Artem Maltsev <artemmaltsevwork@gmail.com>
2026-06-24 13:14:25 +0300

    Sql site Homework

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Data;
+using WebApplication3.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    public StudentsController(AppDbContext context) => _context = context;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() => 
+        Ok(await _context.Students.ToListAsync());
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Student s)
+    {
+        _context.Students.Add(s);
+        await _context.SaveChangesAsync();
+        return Ok(s);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var s = await _context.Students.FindAsync(id);
+        if (s == null) return NotFound();
+        _context.Students.Remove(s);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,16 @@
-using WebApplication2.Services;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IItemService, ItemService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureSql")));
 
 var app = builder.Build();
-
-app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
@@ -29,11 +19,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 app.Run();
