1ff8fcf7ad8bec1f700c57a51f0022db16148e7e
Artem Maltsev <artemmaltsevwork@gmail.com>
2026-06-24 20:10:34 +0300

    Cognitive services Azure

---
+++ Controllers/TranslatorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class TranslateRequest
+{
+    public string Text { get; set; }
+    public string ToLanguage { get; set; }
+}
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranslatorController : ControllerBase
+    {
+        private readonly ServiceTranslator _serviceTranslator;
+
+        public TranslatorController(ServiceTranslator serviceTranslator)
+        {
+            _serviceTranslator = serviceTranslator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Translate([FromBody] TranslateRequest info)
+        {
+            var translated = await _serviceTranslator.TranslateText(info.Text, info.ToLanguage);
+            return Ok(new { original = info.Text, translated = translated });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-using WebApplication2.Services;
+using WebApplication4;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
+// Add services to the container.
+
+builder.Services.AddScoped<ServiceTranslator>();
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IItemService, ItemService>();
-
 var app = builder.Build();
 
-app.UseCors("AllowAll");
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,10 +22,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
